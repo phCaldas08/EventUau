e18916ef1db5e8ce3f1462b5eaf710b45239290d
Pedro Henrique Caldas <phcaldas@MacBook-de-Pedro.local>
2021-09-01 20:44:11 -0300

    carregar dados ao inicializar api

---
+++ Event.Uau.Autenticacao/Event.Uau.Autentificacao.Core/Helpers/DadosFakes/DadosUsuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Event.Uau.Autenticacao.Core.Helpers.DadosFakes
+{
+    public static class DadosUsuario
+    {
+        public static async Task CarregarUsuariosAsync(this IMediator mediator)
+        {
+            var commands = new List<Usuario.Commands.CadastrarUsuario.CadastrarUsuarioCommand>
+            {
+                new Usuario.Commands.CadastrarUsuario.CadastrarUsuarioCommand
+                {
+                    ConfirmarSenha = "@Teste123",
+                    Senha = "@Teste123",
+                    Cpf = "230.146.490-31",
+                    DataNascimento = DateTime.Today.AddYears(-18),
+                    Email = "teste@teste.com",
+                    Nome = "Teste",
+                    SobreNome = "De Oliveira",
+                    Telefone = "(11) 91234-1234",
+                }
+            };
+
+            foreach (var command in commands)
+                await mediator.Send(command);
+        }
+
+    }
+}
