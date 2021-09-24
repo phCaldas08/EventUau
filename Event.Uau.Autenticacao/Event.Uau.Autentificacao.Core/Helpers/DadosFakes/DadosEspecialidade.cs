07f4a7e68e7427ca5c0cfff509ed4361f30b0069
Matheus Victor Almeida <39714637+mva99@users.noreply.github.com>
2021-09-24 19:58:27 -0300

    Adicionar especialidades pré cadastradas (#10)
    Garçom
    Animador
    Churrasqueiro
    Fotógrafo
    Cozinheiro

--- Event.Uau.Autenticacao/Event.Uau.Autentificacao.Core/Helpers/DadosFakes/DadosEspecialidade.cs
+++ Event.Uau.Autenticacao/Event.Uau.Autentificacao.Core/Helpers/DadosFakes/DadosEspecialidade.cs
@@ -11,14 +11,17 @@ namespace Event.Uau.Autenticacao.Core.Helpers.DadosFakes
         {
             var commands = new List<Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand>
             {
-                new Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand
-                {
-                    Descricao = "Garçom"
-                }
+                new Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand {Descricao = "Garçom"},
+                new Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand {Descricao = "Animador"},
+                new Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand {Descricao = "Churrasqueiro"},
+                new Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand {Descricao = "Fotógrafo"},
+                new Especialidade.Commands.CadastrarEspecialidade.CadastrarEspecialidadeCommand {Descricao = "Cozinheiro"}
             };
 
             foreach (var command in commands)
                 await mediator.Send(command);
+
+
         }
     }
 }
