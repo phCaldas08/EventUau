870498ea023685de0e571066919b772dcf43f83c
Matheus Victor Almeida <matheusvictoralmeida1999@gmail.com>
2021-10-16 20:05:37 -0300

    Permitir eventos com inicio e término no mesmo dia

--- Event.Uau.Evento/Event.Uau.Evento.Core/Evento/Commands/CriarEvento/CriarEventoCommandValidator.cs
+++ Event.Uau.Evento/Event.Uau.Evento.Core/Evento/Commands/CriarEvento/CriarEventoCommandValidator.cs
@@ -18,8 +18,8 @@ namespace Event.Uau.Evento.Core.Evento.Commands.CriarEvento
                 .WithMessage("O evento deve ser iniciado com pelo menos 2h de antecedência.");
 
             RuleFor(i => new { i.DataTermino, i.DataInicio })
-                .Must(obj => obj.DataTermino > obj.DataInicio)
-                .WithMessage("A data de início deve ser menor que a data do término.");
+                .Must(obj => obj.DataTermino >= obj.DataInicio)
+                .WithMessage("A data de início não pode ser maior que a data de término do evento.");
 
         }
     }
