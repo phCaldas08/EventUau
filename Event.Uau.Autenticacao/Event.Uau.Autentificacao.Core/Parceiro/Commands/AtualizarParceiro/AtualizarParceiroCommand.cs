d2651e23b0a5fdb3061d12809b738711d12a23ad
Matheus Victor Almeida <matheusvictoralmeida1999@gmail.com>
2021-10-16 17:48:02 -0300

    Atualizar Parceiro

---
+++ Event.Uau.Autenticacao/Event.Uau.Autentificacao.Core/Parceiro/Commands/AtualizarParceiro/AtualizarParceiroCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Event.Uau.Autenticacao.ViewModel.Autenticacao;
+using Event.Uau.Comum.Util.Mediator;
+
+namespace Event.Uau.Autenticacao.Core.Parceiro.Commands.AtualizarParceiro
+{
+    public class AtualizarParceiroCommand : EventUauRequest<ParceiroViewModel>
+    {
+        public int IdUsuario { get; set; }
+        public List<EspecialidadeViewModel> Especialidades { get; set; }
+
+        public decimal ValorHora { get; set; }
+
+        public List<int> IdsEspecialidades { get => Especialidades?.Select(i => i.Id)?.ToList(); }
+    }
+}
